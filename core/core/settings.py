d44f29a165698d265ab5299282252bcef1b70df8
hoesyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-01-04 15:51:18 +0330

    create first super user and migrate models  and config user manager inside setting .py

--- core/core/settings.py
+++ core/core/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig',
-    'accounts.apps.AccountsConfig',
+    # 'blog',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#User manager config
+
+AUTH_USER_MODEL = 'accounts.User'

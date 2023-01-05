d1c479fb0743b915faccb1450a5e2e8acefe3bb3
hoesyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-01-05 15:08:19 +0330

    add manager file in accounts app

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,31 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser , PermissionsMixin
-from django.utils.translation import ungettext_lazy as _ 
+from .managers import UserManager
 
 # Create your models here.
-class UserManager(BaseUserManager):
-    def create_user(self  , email, password, **extra_fields):
-        if not email :
-            raise ValueError(_('The Email must be set'))
-        email = self.normalize_email(email)
-        user = self.model(email = email, **extra_fields)
-        user.set_password(password)
-        user.save()
-        return user
-
-    def create_superuser(self  , email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError(_('Superuser must have is_staff = True'))
-
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError(_('Superuser must have is_supersuer = True'))
-
-        return self.create_user(email, password, **extra_fields)   # مجدد فرستادیم به تابع بالایی فقط با این تفاوت که دیگه مقادیر سوپر یوزرمون فالس نیست
-
 class User (AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length= 255, unique= True)
     is_superuser = models.BooleanField(default= False)

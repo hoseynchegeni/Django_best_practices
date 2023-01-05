94b598356aadeb9ca11c22c48f1d85d405d0ae63
hoesyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-01-05 15:02:18 +0330

    Profile model

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Profile
 from django.contrib.auth.forms import UserCreationForm
 # Register your models here.
 class CustomUserAdmin(UserAdmin):
@@ -32,3 +32,4 @@ class CustomUserAdmin(UserAdmin):
     )
 
 admin.site.register(User, CustomUserAdmin)
+admin.site.register(Profile)

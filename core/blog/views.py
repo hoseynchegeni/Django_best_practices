4ffcf1dccddabbb718d86a8942d83204b67bd360
hoesyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-01-06 13:57:51 +0330

    get context data sample

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic.base import TemplateView
+from .models import Post
 # Create your views here.
+class indexView(TemplateView):
+    template_name  = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs)
+        context['name'] = 'Tahmine Aminzade'
+        context ['test'] = Post.objects.all()
+        return context

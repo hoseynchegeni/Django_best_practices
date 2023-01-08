3b3dc762186ccfadb72dbdfa6ad7eb3de2771dab
hoesyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-01-08 13:18:25 +0330

    list View vol 1

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.views.generic import TemplateView, RedirectView
-from .views import indexView, RedirectToGoogle
+from .views import indexView, RedirectToGoogle, PostList
 app_name = 'blog'
 
 
 urlpatterns = [
     path('', indexView.as_view(), name  = 'ClassBasedView'),
-    path('/google', RedirectView.as_view( url = 'https://google.com/')),
-    path('/class_based_view', RedirectView.as_view(pattern_name = 'blog:ClassBasedView')),
-    path('/google_redirect',RedirectToGoogle.as_view()),
+    path('google', RedirectView.as_view( url = 'https://google.com/')),
+    path('class_based_view', RedirectView.as_view(pattern_name = 'blog:ClassBasedView')),
+    path('google_redirect',RedirectToGoogle.as_view()),
+    path('post/',PostList.as_view(), name= 'PostList')
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView, RedirectView
 from .models import Post
+from django.views.generic import ListView
 # Create your views here.
 class indexView(TemplateView):
     template_name  = 'index.html'
 
     def get_context_data(self, **kwargs):
         context =  super().get_context_data(**kwargs)
-        context['name'] = 'Tahmine Aminzade'
+        context['name'] = 'hoseyn'
         context ['test'] = Post.objects.all()
         return context
 
 class RedirectToGoogle(RedirectView):
     url = 'https://google.com/'
+
+
+
+class PostList(ListView):
+    # model = Post
+    # queryset = Post.objects.all()   
+    def get_queryset(self):
+        posts = Post.objects.filter(status = True)  
+        return posts
+
+    context_object_name = 'posts'
+

a4e1988e0057aa4f7233eeb626da5d4efcc48586
hoesyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-01-10 15:29:51 +0330

    add delete view

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.views.generic import TemplateView, RedirectView
-from .views import indexView, RedirectToGoogle, PostList, PostDetailView,PostCreate, PostEditView
+from .views import indexView, RedirectToGoogle,PostList, PostDetailView,PostCreate, PostEditView,PostDeleteView
 app_name = 'blog'
 
 
@@ -13,4 +13,5 @@ urlpatterns = [
     path('post/<int:pk>/',PostDetailView.as_view(), name = 'PostDetail'),
     path('create/',PostCreate.as_view(), name= 'create'),
     path('post/<int:pk>/edit/', PostEditView.as_view(), name= 'edit'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='delete')
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView, RedirectView
 from .models import Post
-from django.views.generic import ListView, DetailView,FormView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView,FormView, CreateView, UpdateView, DeleteView
 from .forms import PostFrom
 # Create your views here.
 class indexView(TemplateView):
@@ -57,4 +57,9 @@ class PostCreate(CreateView):
 class PostEditView(UpdateView):
     model = Post
     form_class = PostFrom
+    success_url = '/blog/post/'
+
+
+class PostDeleteView(DeleteView):
+    model = Post
     success_url = '/blog/post/'

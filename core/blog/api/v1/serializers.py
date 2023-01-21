64444ce6263724819f7feeefe796b63d1f5d1a4b
hoesyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-01-22 02:08:11 +0330

    Edit category field with to_persention in serializers

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -5,11 +5,17 @@ from ...models import Post, Category
 # class PostSerializer(serializers.Serializer):
 #     id = serializers.IntegerField()
 #     title = serializers.CharField(max_length = 255)
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
 
 
 class PostSerializer(serializers.ModelSerializer):
     # relative_url = serializers.URLField(source = 'get_absolute_api_url', read_only = True)
     absolute_url = serializers.SerializerMethodField() 
+    category = serializers.SlugRelatedField(many = False, slug_field='name',queryset =  Category.objects.all())
+    # category = CategorySerializer()
     class Meta:
         model = Post
         # fields = '__all__' 
@@ -19,9 +25,8 @@ class PostSerializer(serializers.ModelSerializer):
     def get_absolute_url(self, obj):
         request = self.context.get('request')
         return request.build_absolute_uri()
-
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = '__all__'
+    
+    def to_representation(self, instance):
+        rep =  super().to_representation(instance)
+        rep['category'] = CategorySerializer(instance.category).data
+        return rep

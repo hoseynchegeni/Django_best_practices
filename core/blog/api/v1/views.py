1370f4690d27c91bf0f3b8e10e9568fb880d2952
hoesyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-01-24 00:38:47 +0330

    add search fields

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -9,7 +9,7 @@ from rest_framework.views import APIView
 from rest_framework.generics import GenericAPIView, ListAPIView, CreateAPIView,RetrieveUpdateDestroyAPIView
 from .permissions import IsAuthorOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
-
+from rest_framework.filters import SearchFilter
 
 api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticated])
@@ -152,8 +152,9 @@ class PostViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status = True)
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend,SearchFilter]
     filterset_fields = ['category', 'author']
+    search_fields = ['title','content']
 
     @action(methods=['get'], detail= False)
     def get_ok(self, request):

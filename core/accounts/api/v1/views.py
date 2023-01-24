a9b024d1aa39be5d45319db1790a5d2dee4f43dd
hoesyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-01-24 22:54:09 +0330

    add creat and validate function to register serializer

--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -8,12 +8,13 @@ class RegistrationApiView(GenericAPIView):
     serializer_class = RegistrationSerializer
 
     def post(self, request, *args, **kwargs):
-         serializer = RegistrationSerializer(data = request.data)
-         if serializer.is_valid():
+        serializer = RegistrationSerializer(data = request.data)
+        if serializer.is_valid():
               serializer.save()
               data = {
                    'email':serializer.validated_data['email']
               }
               return Response(data, status= status.HTTP_201_CREATED)
+        return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
         
     

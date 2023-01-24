7ecd592f5a5946597f2ff9f20415cdd6246baaff
hoesyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-01-24 19:49:21 +0330

    add api schema downlodad url

--- core/core/urls.py
+++ core/core/urls.py
@@ -26,9 +26,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="Blog API",
       default_version='v1',
-      description="Test description",
+      description="Djago Best Practices APi",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@snippets.local"),
       license=openapi.License(name="BSD License"),
@@ -43,7 +43,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('apu-auth/',include('rest_framework.urls')),
     path('api-docs/',include_docs_urls(title='api-sample')),
-    # re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/api.json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]

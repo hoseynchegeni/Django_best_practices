a8e4f4fc846a01d295b6a8bf87b982564d111e7a
hoesyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-01-26 13:03:10 +0330

    add token login url

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from .views import RegistrationApiView
+from rest_framework.authtoken.views import ObtainAuthToken
 
 
 app_name = 'api-v1'
 
 urlpatterns = [
-    path('registration/', RegistrationApiView.as_view(), name='register')
+    path('registration/', RegistrationApiView.as_view(), name='register'),
+    path('token/login/', ObtainAuthToken.as_view(), name='token-login'),
 ]

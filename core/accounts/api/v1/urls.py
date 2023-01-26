c48ce8b564ae4a151f65fe42749be1cc0c508f64
hoesyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-01-26 14:26:12 +0330

    customize token auth serializ

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
-from .views import RegistrationApiView
-from rest_framework.authtoken.views import ObtainAuthToken
+from .views import RegistrationApiView, CustomAuthToken
+
 
 
 app_name = 'api-v1'
 
 urlpatterns = [
     path('registration/', RegistrationApiView.as_view(), name='register'),
-    path('token/login/', ObtainAuthToken.as_view(), name='token-login'),
+    path('token/login/', CustomAuthToken.as_view(), name='token-login'),
 ]

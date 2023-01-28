bf4e28efb09c90a779b898284d4b333937fee7d6
hoesyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-01-28 13:19:04 +0330

    move jwt urls from root urls to account urls

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from .views import RegistrationApiView, CustomAuthToken,CustomDiscardAuthToken
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 
 
@@ -8,5 +9,8 @@ app_name = 'api-v1'
 urlpatterns = [
     path('registration/', RegistrationApiView.as_view(), name='register'),
     path('token/login/', CustomAuthToken.as_view(), name='token-login'),
-    path('token/logout/', CustomDiscardAuthToken.as_view(), name= 'token-discard')
+    path('token/logout/', CustomDiscardAuthToken.as_view(), name= 'token-discard'),
+    path('jwt/create/', TokenObtainPairView.as_view(), name='jwt-create'),
+    path('jwt/refresh/', TokenRefreshView.as_view(), name='jwt-refresh'),
+    path('jwt/verify/', TokenVerifyView.as_view(), name='jwt-verify'),
 ]
--- core/core/urls.py
+++ core/core/urls.py
@@ -21,7 +21,7 @@ from rest_framework.documentation import include_docs_urls
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
 
 
 
@@ -47,9 +47,6 @@ urlpatterns = [
     path('swagger/api.json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('jwt/token/create/', TokenObtainPairView.as_view(), name='jwt-create'),
-    path('jwt/token/refresh/', TokenRefreshView.as_view(), name='jwt-refresh'),
-     path('jwt/token/verify/', TokenVerifyView.as_view(), name='jwt-verify'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

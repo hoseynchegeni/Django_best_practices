c6d26bb4481676ba657fd47d6cd7f20155e8c0ee
hoesyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-01-28 12:50:32 +0330

    install and add json web token

--- core/core/settings.py
+++ core/core/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'django_filters',
     'drf_yasg',
+    'rest_framework_simplejwt',
+
 ]
 
 MIDDLEWARE = [
@@ -156,5 +158,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
        }

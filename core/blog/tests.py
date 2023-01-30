3e2ab7f3ae0d05f95da3d28225ca92ac50b196f1
hoesyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-01-31 00:33:11 +0330

    writing first test and add .flake file

--- core/blog/tests.py
+++ core/blog/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
-
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse, resolve
+from .views import indexView
 # Create your tests here.
+class TestUrl(SimpleTestCase):
+    def test_blog_index_url_resolve(self):
+        url = reverse('blog:ClassBasedView')
+        self.assertEqual(resolve(url).func.view_class,indexView )

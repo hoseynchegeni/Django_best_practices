f0dd7bb73b7eddcaefff83f77815ef4017f67c9d
hoesyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-02-01 14:50:25 +0330

    remove url.py ;

--- core/blog/api/v1/urls/__init__.py
+++ core/blog/api/v1/urls/__init__.py
@@ -2,5 +2,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("", include("blog.api.v1.urls.post")),
-    path("category", include("blog.api.v1.urls.category")),
+    path("", include("blog.api.v1.urls.category")),
 ]

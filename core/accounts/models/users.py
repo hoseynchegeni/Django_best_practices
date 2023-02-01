50c5e2db26a181682d46f49fa2088c1edd570e7e
hoesyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-02-01 13:32:29 +0330

    move models.py to models folder

--- core/accounts/models/users.py
+++ core/accounts/models/users.py
@@ -1,12 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import (
-    BaseUserManager,
     AbstractBaseUser,
     PermissionsMixin,
 )
 from .managers import UserManager
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+
 
 # Create your models here.
 class User(AbstractBaseUser, PermissionsMixin):
@@ -25,22 +23,3 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
-
-
-class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    image = models.ImageField(blank=True, null=True)
-    desc = models.TextField()
-    created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.user.email
-
-
-@receiver(post_save, sender=User)
-def save_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
